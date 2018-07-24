ba1899bf78384d5f022fe6ff5d193231c534329c
谢新荣 <xiexinrong@xiexinrongdeMacBook-Pro.local>
2018-07-24 13:26:42 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cmq',
+    version='0.2',
+    description='tencentyun cmq python sdk',
+    author='signalxie',
+    author_email='xeixxr@gmail.com',
+    url='https://github.com/coolcoca/cmq-python-sdk',
+    package=['cmq'],
+)
